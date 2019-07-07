27a17e7bdcffcd278f1ddb3d5ca70ff3138c95fa
ItsSkylow <emo.quentin@gmail.com>
2019-07-07 23:07:36 +0200

    [Feat] Add Script v1.0.0

---
+++ sunsets_teleport/client.lua
@@ -0,0 +1,86 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+local player = GetPlayerPed(-1)
+
+Citizen.CreateThread(function ()
+    while true do
+        Citizen.Wait(5)
+        local player = GetPlayerPed(-1)
+        local playerLoc = GetEntityCoords(player)
+
+        for i,location in ipairs(Config.Positions) do
+            teleport_text = location[3]
+            loc1 = {
+                x=location[1][1],
+                y=location[1][2],
+                z=location[1][3],
+                heading=location[1][4]
+            }
+            loc2 = {
+                x=location[2][1],
+                y=location[2][2],
+                z=location[2][3],
+                heading=location[2][4]
+            }
+
+            DrawMarker(1, loc1.x, loc1.y, loc1.z, 0, 0, 0, 0, 0, 0, 1.501, 1.5001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
+            DrawMarker(1, loc2.x, loc2.y, loc2.z, 0, 0, 0, 0, 0, 0, 1.501, 1.5001, 0.5001, 0, 0, 255, 200, 0, 0, 0, 0)
+
+            if CheckPos(playerLoc.x, playerLoc.y, playerLoc.z, loc1.x, loc1.y, loc1.z, 2) then 
+                alert(teleport_text)
+                
+                if IsControlJustReleased(1, Keys[SelectedKey]) then
+                    if IsPedInAnyVehicle(player, true) then
+                        SetEntityCoords(GetVehiclePedIsUsing(player), loc2.x, loc2.y, loc2.z)
+                        SetEntityHeading(GetVehiclePedIsUsing(player), loc2.heading)
+                    else
+                        SetEntityCoords(player, loc2.x, loc2.y, loc2.z)
+                        SetEntityHeading(player, loc2.heading)
+                    end
+                end
+
+            elseif CheckPos(playerLoc.x, playerLoc.y, playerLoc.z, loc2.x, loc2.y, loc2.z, 2) then
+                alert(teleport_text)
+
+                if IsControlJustReleased(1, Keys[SelectedKey]) then
+                    if IsPedInAnyVehicle(player, true) then
+                        SetEntityCoords(GetVehiclePedIsUsing(player), loc1.x, loc1.y, loc1.z)
+                        SetEntityHeading(GetVehiclePedIsUsing(player), loc1.heading)
+                    else
+                        SetEntityCoords(player, loc1.x, loc1.y, loc1.z)
+                        SetEntityHeading(player, loc1.heading)
+                    end
+                end
+            end            
+        end
+    end
+end)
+
+function CheckPos(x, y, z, cx, cy, cz, radius)
+    local t1 = x - cx
+    local t12 = t1^2
+
+    local t2 = y-cy
+    local t21 = t2^2
+
+    local t3 = z - cz
+    local t31 = t3^2
+
+    return (t12 + t21 + t31) <= radius^2
+end
+
+function alert(msg)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(msg)
+    DisplayHelpTextFromStringLabel(0,0,1,-1)
+end
